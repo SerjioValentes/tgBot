7250304f2c3e0bd0c0000873a9c61d8cc6f6607e
SerjioValentes <vasilkamalov@gmail.com>
2024-02-07 13:37:16 +0300

    added menu block

--- index.ts
+++ index.ts
@@ -1,33 +1,97 @@
 import { Bot } from "grammy";
 import { Menu } from "@grammyjs/menu";
+import {
+    type Conversation,
+    type ConversationFlavor,
+    conversations,
+    createConversation,
+  } from "@grammyjs/conversations";
 import 'dotenv/config'
-// require('dotenv').config()
 
-const bot = new Bot(process.env.TG_BOT_API_TOKEN ? process.env.TG_BOT_API_TOKEN : '');
+const bot = new Bot(process.env.TG_BOT_API_TOKEN || '');
 const start = async () => {
+    // --------------------------------------------------------------------------------TODO Typing any
+    type Context = any;
+    type MyContext = Context & ConversationFlavor;
+    type MyConversation = Conversation<MyContext>;
 
-    // Create a simple menu.
-const menu = new Menu("my-menu-identifier")
-.text("A", (ctx) => ctx.reply("You pressed A!")).row()
-.text("B", (ctx) => ctx.reply("You pressed B!"));
-
-// Make it interactive.
-bot.use(menu);
-
-bot.command("start", async (ctx) => {
-// Send the menu.
-await ctx.reply("Check out this menu:", { reply_markup: menu });
-});
+    // async function greeting(conversation: MyConversation, ctx: any) {
+    //     await ctx.reply("Hi there! What is your name?");
+    //     const { message } = await conversation.wait();
+    //     await ctx.reply(`Welcome to the chat, ${message.text}!`);
+    //   }
     
-// Handle the /start command.
-// bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+// const menu = async (ctx: any) => {
 
-// Handle other messages.
-bot.on("message", (ctx) => ctx.reply("Got another message!"));
+    // const mainMenu = {   
+    //     inline_keyboard: [
+    //         // [{ text: 'Some button text 1', callback_data: (ctx: any) => ctx.reply("Hi!").row() }],
+    //         [{ text: 'Some button text 2', callback_data: '2' }],
+    //         [{ text: 'Some button text 3', callback_data: '3' }]
+    //     ]
+    // }
+//     return await ctx ? mainMenu : mainMenu
+// }
+const menu = new Menu("root-menu")
+.text(`Приветствую тебя мой друг`, (ctx) => ctx.reply(`${ctx.from?.last_name} + ${ctx?.from?.first_name}`))
+.submenu("О боте", "about-bot");
+
+const menuB = new Menu("root-menu")
+.text(`muneB`, (ctx) => ctx.reply(`done`));
+
+const menuC = new Menu("root-menu")
+.text(`muneC`, (ctx) => ctx.reply(`done C`));
+
+const settings = new Menu("about-bot")
+.text("Кто я такой?", (ctx) => ctx.reply("lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum"))
+
+  menu.register(settings);
+  bot.use(menu);
+
+// ------------------------------------------------------------------ Handle the /start command.
+bot.command("start", async (ctx) => {
+    // Send the menu.
+    await ctx.reply("Тут есть информация которая тебе нужна?", { 
+        // ------------------------------------------------------------------ Menu markup
+        reply_markup: menu,
+    });
+  });
+
+  bot.command("menu", async (ctx) => {
+    // Send the menu.
+    await ctx.reply(`Check out this menu: ${ctx.from?.last_name} ${ctx?.from?.first_name}`, { 
+        // ------------------------------------------------------------------ Menu markup
+        reply_markup: {   
+              inline_keyboard: [
+                  // [{ text: 'Some button text 1', callback_data: (ctx: any) => ctx.reply("Hi!").row() }],
+                  [{ text: 'Some button text 2', callback_data: '2' }],
+                  [{ text: 'Some button text 3', callback_data: '3' }]
+              ]
+          },
+    });
+
+  });
+
+  bot.on("message", (ctx) => {
+    // console.log(ctx);
+    ctx.msg.text &&  ['нет', 'Нет', 'no', 'No'].includes(ctx.msg.text) ? 
+      (ctx.reply('ok'), {
+        reply_markup: menuB,
+      })
+     : 
+        (ctx.reply("Ну и это хорошо 2"), {
+          reply_markup: {   
+            inline_keyboard: [
+                [{ text: 'Some button text 2', callback_data: '2' }],
+                [{ text: 'Some button text 3', callback_data: '3' }]
+            ]
+        },
+      })
+      
+  });
 
 // Start the bot.
 bot.start();    
 }
-
 start()
 
